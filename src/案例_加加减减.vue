722cfdb77ca549e7ebe0eda6ca2ec0ea06c42f5a
haiou1314 <1871943511@qq.com>
2022-07-02 16:44:27 +0800

    案例加加减减

---
+++ src/案例_加加减减.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, index) in arr" :key="index">
+        <span>{{ item }}</span>
+        <button @click="del(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="add">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 5, 3],
+    }
+  },
+  methods: {
+    add() {
+      this.arr.push(Math.floor(Math.random() * 10))
+    },
+    del(id){
+      let index =this.arr.findIndex((item)=>item==id)
+      this.arr.splice(index,1)
+    }
+  },
+}
+</script>
