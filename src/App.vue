03234ad02b081d2448df77761af03a7c249403b0
haiou1314 <1871943511@qq.com>
2022-07-02 17:21:32 +0800

    求和

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <template>
   <div>
-    <p>请选择你要购买的书籍</p>
-    <ul>
-      <li v-for="(item, index) in arr" :key="index">
-        <span>{{ item['name'] }}</span>
-        <button @click="sum(index)">买书</button>
-      </li>
-    </ul>
-    <table border="1" width="500" cellspacing="0">
-      <tr v-for="(item, index) in arr" :key="index">
-        <th>{{ index + 1 }}</th>
-        <th>{{ item.name }}</th>
-        <th>{{ item.price }}</th>
-        <th>{{ item.count }}</th>
-        <th>{{ item.price * item.count }}</th>
-      </tr>
-    </table>
-    <p>总价格为:{{ count }}</p>
+    <div v-for="(item, index) in arr" :key="index" >
+      <input type="checkbox" v-model="newArr" :value="item" />
+      <span>{{ item }}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{ sum }}</p>
   </div>
 </template>
 
@@ -24,41 +12,19 @@
 export default {
   data() {
     return {
-      arr: [
-        {
-          name: '水浒传',
-          price: 107,
-          count: 0,
-        },
-        {
-          name: '西游记',
-          price: 192,
-          count: 0,
-        },
-        {
-          name: '三国演义',
-          price: 219,
-          count: 0,
-        },
-        {
-          name: '红楼梦',
-          price: 178,
-          count: 0,
-        },
-      ],
-    }
-  },
-  methods: {
-    sum(id) {
-      this.arr[id]['count']++
-    },
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      newArr: [], 
+    };
   },
   computed: {
-    count() {
-      return this.arr.reduce((sum, next) => {
-        return (sum += next.price * next.count)
-      }, 0)
+    sum() {
+      return this.newArr.reduce((sum, val) => {
+        return (sum += val);
+      }, 0);
     },
   },
-}
+};
 </script>
+
+<style>
+</style>
